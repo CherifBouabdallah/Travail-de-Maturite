787eb9e298cfd2168999b7150dc9a4108257b95c
Cherif <chena.bou16@gmail.com>
2023-05-09 21:26:25 +0200

    Added Headers !

--- cleaned/Simulator.py
+++ cleaned/Simulator.py
@@ -8,10 +8,10 @@ Angle_of_Arrival_Degrees = float(Angle_of_Arrival_Degrees)
 
 Angle_of_Arrival_Radians = math.radians(Angle_of_Arrival_Degrees)
 
-Refracion_index_1 = input("Donne moi l'indice de réfraction du milieu 1 (air = 1; eau = 1,333; plexiglass = 1,51) : ")
+Refracion_index_1 = input("Donne moi l'indice de réfraction du milieu 1 (air = 1; eau = 1.333; plexiglass = 1.51) : ")
 Refracion_index_1 = float(Refracion_index_1)
 
-Refracion_index_2 = input("Donne moi l'indice de réfraction du milieu 2 (air = 1; eau = 1,333; plexiglass = 1,51) : ")
+Refracion_index_2 = input("Donne moi l'indice de réfraction du milieu 2 (air = 1; eau = 1.333; plexiglass = 1.51) : ")
 Refracion_index_2 = float(Refracion_index_2)
 
 #The fun part !
@@ -31,14 +31,26 @@ else:
 pygame.init()
 
 caption = "Refraction Simulator"
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((1000, 700))
 pygame.display.set_caption(caption)
+screen_width, screen_height = 1000, 700
 
 black = (0, 0, 0)
 white = (255, 255, 255)
 
-font = pygame.font.SysFont("Calibri", 72)
-text = font.render((str(Angle_of_Refraction_Degrees_Rounded)), True,  white)
+Bigfont = pygame.font.SysFont("Calibri", 72)
+font = pygame.font.SysFont("Calibri", 52)
+Smallfont = pygame.font.SysFont("Calibri", 20)
+
+Angle_of_Refraction_Display = Bigfont.render((str(Angle_of_Refraction_Degrees_Rounded)), True,  white)
+Angle_of_Arrival_Display = font.render((str(Angle_of_Arrival_Degrees)), True,  white)
+Refracion_index_1_Display = font.render((str(Refracion_index_1)), True,  white)
+Refracion_index_2_Display = font.render((str(Refracion_index_2)), True,  white)
+
+Angle_of_Refraction_Display_Header = font.render((str("Angle réfracté")), True,  white)
+Angle_of_Arrival_Display_Header = Smallfont.render(("Angle d'arrivée"), True,  white)
+Refracion_index_1_Display_Header = Smallfont.render(("Indice de réfraction du milieu 1"), True,  white)
+Refracion_index_2_Display_Header = Smallfont.render(("Indice de réfraction du milieu 2"), True,  white)
 
 # Main Loop
 done = False
@@ -48,7 +60,16 @@ while not done:
             done = True
     screen.fill(black)
 
-    screen.blit(text, (800 // 2 - text.get_width() // 2, 600 // 2 - text.get_height() // 2))
+    #Shows things on screen
+    screen.blit(Angle_of_Refraction_Display, (screen_width // 2 - Angle_of_Refraction_Display.get_width() // 2, screen_height // 2 - Angle_of_Refraction_Display.get_height() // 2))
+    screen.blit(Angle_of_Arrival_Display, (screen_width // 2 - Angle_of_Arrival_Display.get_width() // 2, screen_height // 5 - Angle_of_Arrival_Display.get_height() // 5))
+    screen.blit(Refracion_index_1_Display, (screen_width // 8 - Refracion_index_1_Display.get_width() // 8, screen_height // 5 - Refracion_index_1_Display.get_height() // 5))
+    screen.blit(Refracion_index_2_Display, (screen_width // (8/7) - Refracion_index_2_Display.get_width() // (8/7), screen_height // 5 - Refracion_index_2_Display.get_height() // 5))
+
+    screen.blit(Angle_of_Refraction_Display_Header, (screen_width // 2 - Angle_of_Refraction_Display_Header.get_width() // 2, screen_height // 2.5 - Angle_of_Refraction_Display_Header.get_height() // 2.5))
+    screen.blit(Angle_of_Arrival_Display_Header, (screen_width // 2 - Angle_of_Arrival_Display_Header.get_width() // 2, screen_height // 10 - Angle_of_Arrival_Display_Header.get_height() // 10))
+    screen.blit(Refracion_index_1_Display_Header, (screen_width // 8 - Refracion_index_1_Display_Header.get_width() // 8, screen_height // 10 - Refracion_index_1_Display_Header.get_height() // 10))
+    screen.blit(Refracion_index_2_Display_Header, (screen_width // (8/7) - Refracion_index_2_Display_Header.get_width() // (8/7), screen_height // 10 - Refracion_index_2_Display_Header.get_height() // 109))
     pygame.display.update()
     pygame.time.Clock().tick(30)
 
@@ -58,7 +79,7 @@ pygame.quit()
 
 # TO ADD : 
 
-# go from tkiner to pygame
+# show RI and Angle on screen
 # A slider to choose angle and RI
 # the laser
 # the transparent objects
@@ -67,5 +88,5 @@ pygame.quit()
 # A window with the output angle
 # In case of a reflexion !
 # # add comas
-
+# go from tkiner to pygame
 
