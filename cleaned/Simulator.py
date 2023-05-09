ec75b90ce5f3bb734b2198118bd1e20d0c68a5dc
Cherif_GYYV <chena.bou16@gmail.com>
2023-05-09 15:39:01 +0200

    Reflexion + commas supported!

--- cleaned/Simulator.py
+++ cleaned/Simulator.py
@@ -4,24 +4,36 @@ import tkinter as tk
 #Asking for things
 
 Angle_of_Arrival_Degrees = input("Donne moi un angle d'arivée : ")
-Angle_of_Arrival_Degrees = int(Angle_of_Arrival_Degrees)
+Angle_of_Arrival_Degrees = float(Angle_of_Arrival_Degrees)
 
 Angle_of_Arrival_Radians = math.radians(Angle_of_Arrival_Degrees)
 
 Refracion_index_1 = input("Donne moi l'indice de réfraction du milieu 1 (air = 1; eau = 1,333; plexiglass = 1,51) : ")
-Refracion_index_1 = int(Refracion_index_1)
+Refracion_index_1 = float(Refracion_index_1)
 
 Refracion_index_2 = input("Donne moi l'indice de réfraction du milieu 2 (air = 1; eau = 1,333; plexiglass = 1,51) : ")
-Refracion_index_2 = int(Refracion_index_2)
+Refracion_index_2 = float(Refracion_index_2)
 
 #The fun part !
 
-Angle_of_Refraction_Radians = math.asin((Refracion_index_1 * math.sin(Angle_of_Arrival_Radians)) / Refracion_index_2)
+First_Part_of_Calculation = ((Refracion_index_1 * math.sin(Angle_of_Arrival_Radians)) / Refracion_index_2)
 
-Angle_of_Refraction_Degrees = math.degrees(Angle_of_Refraction_Radians)
-Angle_of_Refraction_Degrees_Rounded = round(Angle_of_Refraction_Degrees)
+if -1 <= First_Part_of_Calculation <= 1:
+    Angle_of_Refraction_Radians = math.asin((Refracion_index_1 * math.sin(Angle_of_Arrival_Radians)) / Refracion_index_2)
 
-#print("l'angle réfléchi est de : {Angle_of_Refraction_Degrees_Rounded}")
+    Angle_of_Refraction_Degrees = math.degrees(Angle_of_Refraction_Radians)
+    Angle_of_Refraction_Degrees_Rounded = round(Angle_of_Refraction_Degrees, 2)
+else:
+    Angle_of_Refraction_Degrees_Rounded = ('Reflexion')
+    
+
+
+
+
+
+#print(((Refracion_index_1 * math.sin(Angle_of_Arrival_Radians)) / Refracion_index_2))
+
+#Create window !
 
 window = tk.Tk()
 window.geometry("800x600")
@@ -44,10 +56,14 @@ window.mainloop()
 
 # TO ADD : 
 
-# In case of a reflexion !
+
 # A slider to choose angle and RI
 # the laser
 # the transparent objects
 
 # DONE :
-# A window with the output angle 
+# A window with the output angle
+# In case of a reflexion !
+# # add comas
+
+
