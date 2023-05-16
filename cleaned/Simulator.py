4a1a852da38c9092955b05cb4359c70f25c571d8
Cherif <chena.bou16@gmail.com>
2023-05-16 19:58:06 +0200

    3 sliders done !

--- cleaned/Simulator.py
+++ cleaned/Simulator.py
@@ -2,7 +2,7 @@ import math
 import pygame
 
 
-
+'''
 Angle_of_Arrival_Degrees = input("Donne moi un angle d'arrivée : ")
 Angle_of_Arrival_Degrees = float(Angle_of_Arrival_Degrees)
 
@@ -28,6 +28,8 @@ if -1 <= First_Part_of_Calculation <= 1:
 else:
     Angle_of_Refraction_Degrees_Rounded = ('Reflexion')
     Angle_of_Refraction_Degrees = ('Reflexion')
+'''
+
 
 
 #Create window !
@@ -62,6 +64,13 @@ slider_RI1_y = ((screen_height // 5 - slider_RI2_height // 5))
 slider_RI1_value = 0.5
 slider_RI1_grabbed = False
 
+slider_AOA_width = 200
+slider_AOA_height = 20
+slider_AOA_x = (screen_width // 2 - slider_AOA_width // 2)
+slider_AOA_y = ((screen_height // 5 - slider_AOA_height // 5))
+slider_AOA_value = 0.5
+slider_AOA_grabbed = False
+
 
 
 Angle_of_Refraction_Display = Bigfont.render((str(Angle_of_Refraction_Degrees)), True,  white)
@@ -88,11 +97,17 @@ while not done:
                     slider_RI2_grabbed = True
                 if slider_RI1_x <= mouse_x <= slider_RI1_x + slider_RI1_width and slider_RI1_y <= mouse_y <= slider_RI1_y + slider_RI1_height:
                     slider_RI1_grabbed = True
+                if slider_AOA_x <= mouse_x <= slider_AOA_x + slider_AOA_width and slider_AOA_y <= mouse_y <= slider_AOA_y + slider_AOA_height:
+                    slider_AOA_grabbed = True
+
         elif event.type == pygame.MOUSEBUTTONUP:
             if event.button == 1:
                 slider_RI2_grabbed = False
             if event.button == 1:
                 slider_RI1_grabbed = False
+            if event.button == 1:
+                slider_AOA_grabbed = False
+
         elif event.type == pygame.MOUSEMOTION:
             if slider_RI2_grabbed:
                 mouse_x, mouse_y = event.pos
@@ -102,6 +117,10 @@ while not done:
                 mouse_x, mouse_y = event.pos
                 slider_RI1_value = (mouse_x - slider_RI1_x) / slider_RI1_width
                 slider_RI1_value = max(0.01, min(slider_RI1_value, 1))
+            if slider_AOA_grabbed:
+                mouse_x, mouse_y = event.pos
+                slider_AOA_value = (mouse_x - slider_AOA_x) / slider_AOA_width
+                slider_AOA_value = max(0, min(slider_AOA_value, 1))
 
     if -1 <= First_Part_of_Calculation <= 1:
         Angle_of_Refraction_Radians = math.asin((Refraction_index_1 * math.sin(Angle_of_Arrival_Radians)) / Refraction_index_2)
@@ -114,10 +133,13 @@ while not done:
 
     Refraction_index_2 = round(slider_RI2_value * 2, 2)
     Refraction_index_1 = round(slider_RI1_value * 2, 2)
+    Angle_of_Arrival_Radians = round(slider_AOA_value * 90, 2)
+    
     result = round(Angle_of_Refraction_Degrees, 2)
 
     Refraction_index_2_text = Smallfont.render(str(Refraction_index_2), True, white)
     Refraction_index_1_text = Smallfont.render(str(Refraction_index_1), True, white)
+    Angle_of_Arrival_Radians_text = Smallfont.render(str(Angle_of_Arrival_Radians), True, white)
     result_text = font.render(str(result), True, white)
 
     pygame.draw.rect(screen, gray, [slider_RI2_x, slider_RI2_y, slider_RI2_width, slider_RI2_height])
@@ -126,9 +148,11 @@ while not done:
     pygame.draw.rect(screen, gray, [slider_RI1_x, slider_RI1_y, slider_RI1_width, slider_RI1_height])
     pygame.draw.rect(screen, white, [slider_RI1_x + slider_RI1_value * slider_RI1_width - 5, slider_RI1_y, 10, slider_RI1_height])
 
+    pygame.draw.rect(screen, gray, [slider_AOA_x, slider_AOA_y, slider_AOA_width, slider_AOA_height])
+    pygame.draw.rect(screen, white, [slider_AOA_x + slider_AOA_value * slider_AOA_width - 5, slider_AOA_y, 10, slider_AOA_height])
+
 
     #Shows things on screen
-    screen.blit(Angle_of_Arrival_Display, (screen_width // 2 - Angle_of_Arrival_Display.get_width() // 2, screen_height // 5 - Angle_of_Arrival_Display.get_height() // 5))
 
     screen.blit(Angle_of_Refraction_Display_Header, (screen_width // 2 - Angle_of_Refraction_Display_Header.get_width() // 2, screen_height // 2.5 - Angle_of_Refraction_Display_Header.get_height() // 2.5))
     screen.blit(Angle_of_Arrival_Display_Header, (screen_width // 2 - Angle_of_Arrival_Display_Header.get_width() // 2, screen_height // 10 - Angle_of_Arrival_Display_Header.get_height() // 10))
@@ -141,6 +165,9 @@ while not done:
 
     screen.blit(Refraction_index_1_text, [slider_RI1_x - Refraction_index_1_text.get_width() - 10, slider_RI1_y + slider_RI1_height // 2 - Refraction_index_1_text.get_height() // 2])
     screen.blit(result_text, (screen_width // 2 - Refraction_index_1_Display.get_width() // 2, screen_height // 2 - Refraction_index_1_Display.get_height() // 2))   
+
+    screen.blit(Angle_of_Arrival_Radians_text, [slider_AOA_x - Angle_of_Arrival_Radians_text.get_width() - 10, slider_AOA_y + slider_AOA_height // 2 - Angle_of_Arrival_Radians_text.get_height() // 2])
+    screen.blit(result_text, (screen_width // 2 - Angle_of_Arrival_Display.get_width() // 2, screen_height // 2 - Angle_of_Arrival_Display.get_height() // 2))   
    
    
    
