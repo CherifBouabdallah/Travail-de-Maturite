58b9208cb6da633a269b12f9baa092a22e9751b7
Cherif B <chrif.bouabdallah@gmail.com>
2023-07-11 13:20:22 +0200

    new file to add objects

---
+++ everything/Re_write_sim.py
@@ -0,0 +1,17 @@
+import pygame
+import math
+
+pygame.init()
+caption = "Refraction Simulator"
+screen_width, screen_height = 1000, 700
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption(caption)
+
+#Definition of colors and font sizes
+black = (0, 0, 0)
+white = (255, 255, 255)
+gray = (128, 128, 128)
+
+Bigfont = pygame.font.SysFont("Calibri", 72)
+font = pygame.font.SysFont("Calibri", 52)
+Smallfont = pygame.font.SysFont("Calibri", 20)
