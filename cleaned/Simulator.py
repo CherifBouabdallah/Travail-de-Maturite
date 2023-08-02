d14c653201a78b862910de11dae55001c2ab53bd
Cherif <chena.bou16@gmail.com>
2023-08-02 14:07:43 +0200

    no more separated files for now

--- cleaned/Simulator.py
+++ cleaned/Simulator.py
@@ -23,6 +23,49 @@ slider_height = 20
 mouse_x = 0
 mouse_y = 0
 
+class Slider:
+    def __init__(self, value, pos_x, pos_y, min_value, max_value, round):
+        self.value = value
+        self.grabbed = False
+        self.width = slider_width
+        self.height = slider_height
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.header = 0
+        self.align_x = 0
+        self.align_y = 0
+        self.start_value = 0
+        self.min_value = min_value
+        self.max_value = max_value
+        self.round = round
+
+    def update_grabbed(self, mouse_x, mouse_y):
+        mouse_x, mouse_y = event.pos
+        if self.pos_x <= mouse_x <= self.pos_x + self.width and self.pos_y <= mouse_y <= self.pos_y + self.height:
+            self.grabbed = True
+
+    def update_released(self):
+        if event.button == 1:
+            self.grabbed = False
+
+    def update_motion(self, mouse_x):
+        if self.grabbed:
+            mouse_x, _ = event.pos
+            self.value = (mouse_x - self.pos_x) / self.width
+            self.value = max(self.min_value, min(self.value, self.max_value))
+
+    def calculation_value(self, multiplyer):
+        self.real_value = round(self.value * multiplyer, self.round)
+
+    def draw_slider(self):
+        pygame.draw.rect(screen, gray, [self.pos_x, self.pos_y, self.width, self.height])
+        pygame.draw.rect(screen, white, [self.pos_x + self.value * self.width - 5, self.pos_y, 10, self.height])
+
+    def render_header(self, header):
+        self.header = Smallfont.render(header, True, (255, 255, 255))
+
+    def blit_header(self, align_x, align_y):
+        screen.blit(self.header, (screen_width // align_x - self.header.get_width() // align_x, screen_height // align_y - self.header.get_height() // align_y))
 
 slider_RI1 = Slider(0.5, screen_width // 6 - slider_width // 6, screen_height // 10 - slider_height // 10, -1, 1, 2) #this is experimental, remove when final ! (the -1 is exp)
 slider_RI2 = Slider(0.5, screen_width // (1.2) - slider_width // (1.2), screen_height // 10 - slider_height // 10, 0, 1, 2)
